664dcf0b035897ee00dec7f49e2628c22f8ce6c5
KaterynaHarmash <ekaterinagarmash1993@gmail.com>
2023-12-06 19:19:49 +0200

    for send to mentor

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import * as _ from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const currTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currTime).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', _(function (event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds.toFixed());
+}, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import * as _ from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input[name="email"]');
+const messageRef = document.querySelector('textarea[name="message"]');
+const data = {};
+let data2storage;
+let dataFromStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (dataFromStorage) {
+    if (dataFromStorage.email) {
+        emailRef.value = dataFromStorage.email;
+}
+    if (dataFromStorage.message) {
+        messageRef.value = dataFromStorage.message;
+}
+}
+
+form.addEventListener('input', _(function (event) {
+    data.email = emailRef.value;
+    data.message = messageRef.value;
+//Зробила саме так, тому що через data[event.target.value]
+// при редагуванні тільки 1 поля
+//значення іншого очищувалось з локал сторейдж
+//(бо запис спрацьовував тільки для поля де проходив івент "інпут")
+    
+    data2storage = JSON.stringify(data);
+    localStorage.setItem('feedback-form-state', data2storage);
+}, 500));
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    dataFromStorage = JSON.parse(localStorage.getItem('feedback-form-state'));
+    console.log(dataFromStorage);
+  localStorage.clear();
+  form.reset();
+});
