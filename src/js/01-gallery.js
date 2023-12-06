aab898f8149f453c793d42a02fd9ef0759270a7a
Kateryna Harmash <139209869+KaterynaHarmash@users.noreply.github.com>
2023-12-06 12:47:02 +0200

    ...

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import"simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryRef = document.querySelector('ul.gallery');
+createGallery(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionSelector: '.gallery img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
 
-console.log(galleryItems);
+function createGallery(items) {
+    const galleryListMarkup = items.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+    </li>`
+    });
+
+    galleryRef.insertAdjacentHTML('beforeend', galleryListMarkup.join(''));
+}
